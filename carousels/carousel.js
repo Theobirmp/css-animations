2125a82bc355fd9e552c2f160ce716afa6aca245
Theobirmp <theobirmpil@gmail.com>
2022-07-24 18:18:13 +0300

    init

---
+++ carousels/carousel.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
